f5780841eaa2536ba8ae05890324d0597f82a6df
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2025-11-26 01:19:54 +0530

    initial commit

---
+++ SQLQuery1.sql
@@ -0,0 +1,5 @@
+
+use db_Churn;
+GO
+SELECT * FROM dbo.stg_Churn;
+
---
+++ SQLQuery2.sql
@@ -0,0 +1,22 @@
+use db_Churn;
+GO
+select Gender,Count(Gender)as TotalCount,
+Count(Gender)*100.0/(select Count(*)From stg_Churn)as Percentage
+from stg_Churn
+Group by Gender
+
+Select Contract,Count(Contract)As TotalCount,
+Count(Contract)*1.0/(Select Count(*)From stg_Churn)As Percentage
+from stg_Churn
+Group by Contract
+
+SELECT Customer_Status, Count(Customer_Status) as TotalCount, Sum(Total_Revenue) as TotalRev,
+Sum(Total_Revenue) / (Select sum(Total_Revenue) from stg_Churn) * 100  as RevPercentage
+from stg_Churn
+Group by Customer_Status
+
+SELECT State, Count(State) as TotalCount,
+Count(State) * 100.0 / (Select Count(*) from stg_Churn)  as Percentage
+from stg_Churn
+Group by State
+Order by Percentage desc
